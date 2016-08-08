d8ca50729c01c817363d91f52def654237bfdcf9
Anthony Puech <anthony30puech@yahoo.fr>
2016-08-08 15:04:42 +0200

    CRUD routes added to Products and the def in the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,22 @@
 class ProductsController < ApplicationController
-
-
     def index
-
         @products = Product.all
-
     end
 
     def show
       @product = Product.find(params[:id])
     end
+    def create
 
+    end
+    def edit
+
+    end
+    def update
+
+    end
+
+    def destroy
+
+    end
   end
