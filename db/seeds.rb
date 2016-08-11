2cb5e5e8bb7cc9ef3e14671770541052143fbeae
Anthony Puech <anthony30puech@yahoo.fr>
2016-08-11 14:11:30 +0200

    new user story added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 
 Product.destroy_all
 
+
+require "open-uri"
+require "json"
+
 url = "https://api.producthunt.com/v1/posts"
 headers = {
   "Authorization" => "Bearer 307618982c4371c20c359d74e3e4b325472fd0ea4be6fab766944b31358265ec"
@@ -16,5 +20,5 @@ headers = {
 posts = JSON.parse(open(url, headers).read)["posts"]
 
 posts.each do |post|
-  Product.create!(name: post["name"],tagline: post["tagline"],url: post["discussion_url"])
+  Product.create!(name: post["name"],tagline: post["tagline"],url: post["discussion_url"][1.15])
 end
